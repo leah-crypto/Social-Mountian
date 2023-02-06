b329f7fb567bb1ba5ea72eed659f7bdcab343af7
Leah Roberson <77908412+leah-crypto@users.noreply.github.com>
2023-02-06 17:26:09 -0600

    update dj helped

--- react-proj-4/starter/server/middleware/isAuthenticated.js
+++ react-proj-4/starter/server/middleware/isAuthenticated.js
@@ -1,37 +1,37 @@
-require('dotenv').config()
-const jwt = require('jsonwebtoken')
-const {JWT_SECRET} = process.env
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = process.env;
 
 module.exports = {
-    isAuthenticated: (req, res, next) => {
-        const headerToken = req.get('Authorization')
-        
-        console.log(JWT_SECRET, headerToken, "header toooken")
-        if (!headerToken) {
-            console.log('ERROR IN auth middleware')
-            res.sendStatus(401)
-        }
+  isAuthenticated: (req, res, next) => {
+    const headerToken = req.get("Authorization");
 
-        let token
-
-        try {
-            token = jwt.verify(headerToken, JWT_SECRET, (err, decoded) => {
-                if(err){
-                    console.log(decoded);
-                }
-            })
-        } catch (err) {
-            err.statusCode = 500
-            console.log("coming from here")
-            throw err
-        }
-
-        if (!token) {
-            const error = new Error('Not authenticated.')
-            error.statusCode = 401
-            throw error
-        }
-
-        next()
+    console.log(JWT_SECRET, headerToken, "header toooken");
+    if (!headerToken) {
+      console.log("ERROR IN auth middleware");
+      res.sendStatus(401);
     }
-}
+
+    let token;
+
+    try {
+      token = jwt.verify(headerToken, JWT_SECRET, (err, decoded) => {
+        if (err) {
+          console.log(err, "err");
+        }
+      });
+    } catch (err) {
+      err.statusCode = 500;
+      console.log("coming from here");
+      throw err;
+    }
+
+    if (!token) {
+      const error = new Error("Not authenticated.");
+      error.statusCode = 401;
+      throw error;
+    }
+
+    next();
+  },
+};
